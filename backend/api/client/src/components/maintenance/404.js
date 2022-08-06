db1ec2a712ecabc160756b5459331925ba4ef90f
Sharifdesigns22 <shariftarver@gmail.com>
2022-08-06 16:05:35 -0400

    updated redirect in toml file

---
+++ backend/api/client/src/components/maintenance/404.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function notFound404() {
+  return (<>
+  <head class="404Header">
+      404 Not Found
+  </head>
+
+  <h1> Unfortunately We could not find this resource please check back in 30 minutes.</h1>
+
+
+  <div>
+      We apologize for the inconvenience.
+  </div>
+  
+  </>);
+}
---
+++ backend/api/client/src/components/maintenance/maintenance.js
@@ -0,0 +1,43 @@
+import React from "react";
+import "/Users/shariftarver/Desktop/Founder/Abena Dms/abena-dms/backend/api/client/src/maintenance.css";
+import { Routes, Route } from "react-router-dom";
+
+// to do: add a profiler in prod to see where I can improve performance
+
+export default function Maintenance() {
+  return (
+    <>
+      <head>
+        <title>Performing Maintenance</title>
+      </head>
+
+      <body>
+        <div id="article">
+          <h1>Abena Dms is currently under maintenance</h1>
+          <h1>Our site is getting a little tune up and some love.</h1>
+          <div>
+            <p>
+              We apologize for the inconvenience, but we're performing some
+              maintenance. You can still contact us at{" "}
+              <a href="mailto:info@abena-dms.com">info@abena-dms.com</a>. We'll
+              be back up soon!
+            </p>
+            <br />
+            <p>— Abena DMS Team</p>
+
+            <br />
+
+            <img
+              src="https://user-images.githubusercontent.com/22308837/157813235-24e05afb-589d-4452-a858-f1fca1e782c7.png"
+              width="200"
+            />
+          </div>
+        </div>
+      </body>
+
+      <div />
+      <div />
+      <div />
+    </>
+  );
+}
